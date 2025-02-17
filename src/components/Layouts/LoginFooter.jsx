6f357d20d8b93bdad106ae5418b43ba7c6f65a75
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2025-02-17 10:56:02 +0530

    update: login and redirection after login

--- src/components/Layouts/LoginFooter.jsx
+++ src/components/Layouts/LoginFooter.jsx
@@ -3,14 +3,14 @@ import { FooterLink } from "../common/Link";
 
 export const Footer = () => {
     return(
-        <div className="py-2 absolute bottom-10">
-            <div className="flex w-full gap-x-8 py-6">
+        <div className="py-2 absolute bottom-5 md:bottom-10">
+            <div className="flex justify-center w-full gap-x-8 py-6">
                 <FooterLink to={'/'} text="Terms & Condition"></FooterLink>
                 <FooterLink to={'/'} text="Privacy"></FooterLink>
                 <FooterLink to={'/'} text="Help"></FooterLink>
             </div>
             <div>
-                <p className="text-sm text-gray-500">&copy; 2024 Bright Academy Government Exam Coaching Center</p>
+                <p className="text-sm text-gray-500">&copy; 2025 HypeSquad - MAM College of Engineering and Technology, Siruganur</p>
             </div>
         </div>
     );
