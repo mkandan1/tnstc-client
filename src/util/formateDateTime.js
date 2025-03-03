4e1e8db431e23bd7b53deaa4db188fbf1ef3cc82
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2025-03-03 14:37:23 +0530

    fix: different time in schedules

--- src/util/formateDateTime.js
+++ src/util/formateDateTime.js
@@ -1,7 +1,8 @@
+import { DateTime } from "luxon";
+
 export const formatDateTime = (dateString) => {
-    if (!dateString) return "";
-    const date = new Date(dateString);
-    return new Date(date.getTime() - date.getTimezoneOffset() * 60000)
-      .toISOString()
-      .slice(0, 16);
-  };
+  if (!dateString) return "";
+
+  return DateTime.fromISO(dateString, { zone: "utc" })
+    .toFormat("yyyy-MM-dd'T'HH:mm"); // Keep in UTC, no timezone shift
+};
