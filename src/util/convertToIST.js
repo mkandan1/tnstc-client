9574f4700059039bd429c0bced9c04e679529add
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2025-03-03 14:31:38 +0530

    fix: different time in schedules

--- src/util/convertToIST.js
+++ src/util/convertToIST.js
@@ -1,26 +1,17 @@
+import { DateTime } from "luxon";
+
 export const convertToIST = (isoString) => {
   if (!isoString) return "Invalid Date";
 
-  return new Date(isoString).toLocaleString("en-IN", {
-    timeZone: "Asia/Kolkata",
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: true,
-  });
+  return DateTime.fromISO(isoString, { zone: "utc" })
+    .setZone("Asia/Kolkata")
+    .toFormat("dd-MM-yyyy hh:mm:ss a");
 };
+
 export const getISTTime = (isoString) => {
-  if (!isoString) return "-"; // Handle null or undefined values
+  if (!isoString) return "-";
 
-  const date = new Date(isoString);
-  if (isNaN(date.getTime())) return "-"; // Handle invalid dates
-
-  return date.toLocaleTimeString("en-IN", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-  });
+  return DateTime.fromISO(isoString, { zone: "utc" })
+    .setZone("Asia/Kolkata")
+    .toFormat("hh:mm a");
 };
